14f781ea2e08df4e904c1888ecf63bfca34ddba3
Mark Chipman <markchipman@gmail.com>
2017-08-29 23:36:24 -0600

    Added unit tests

---
+++ WPFChallenge/UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cf55b1bd-874a-43af-9ab0-d17e915e6aee")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WPFChallenge/UnitTests/WPFUnitTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class WPFUnitTests
+    {
+        public Mock<IMessageBox> _messageBox;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _messageBox = new Mock<IMessageBox>(); // Instantiate the mock message box component
+        }
+
+        [TestMethod]
+        public void Test()
+        {
+            _messageBox.Setup(m => m.Show("TEST"));
+
+            var sut = new SUT(_messageBox.Object);
+
+            // Act
+            sut.TestMessageBoxShowMethod();
+
+            // Verify
+            _messageBox.Verify();
+        }
+
+    }
+    public interface IMessageBox
+    {
+        void Show(String message);
+    }
+
+    public class SUT
+    {
+        private IMessageBox _messageBox;
+
+        public SUT(IMessageBox messageBox)
+        {
+            _messageBox = messageBox;
+        }
+
+        public void TestMessageBoxShowMethod()
+        {
+            _messageBox.Show("TEST");
+        }
+    }
+
+
+}
